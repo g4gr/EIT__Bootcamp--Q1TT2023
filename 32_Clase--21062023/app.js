0ad00991111695407e9f8606c8653335fb403990
Gaby <76602751+gagr-dev@users.noreply.github.com>
2023-06-21 21:04:28 -0300

    app.js( Feat ): Primeros pasos con JS
    Clase del dia 21/06/2023

---
+++ 32_Clase--21062023/app.js
@@ -0,0 +1,54 @@
+// Tipos de datos
+
+// Booleans : true / false
+console.log(true)
+console.log(false)
+
+// Numericos : Enteros / Flotantes
+console.log(-1123)
+console.log(-1123.90)
+
+// Valores infinitos
+console.log(40 / -0)
+console.log(40 / 0)
+
+// Not a Number = NaN
+console.log(NaN)
+console.log(!NaN)
+
+// Valores Nulos
+console.log(null)
+
+// Valores Undefined
+console.log(undefined)
+
+// String ( cadenas de textos )
+console.log('Hola utilizando comillas\n simples')
+console.log("Hola utilizando\n comillas dobles")
+console.log(`
+Hola utilizando comillas 
+invertidas ${40 * 5} backtick
+` )
+
+// Symbol
+console.log()
+
+// Objetos
+console.log(Object)
+// var objetos = { "clave" : "valor" , "clave1" : "valor"  }
+
+// Arreglos
+// var arreglo = [ "" , "" , "" ] ;
+
+// ------------------------------
+// VARIABLES
+
+// console.log( variebleVar )
+
+let variableLet = 'String';
+
+
+const variableConst = true;
+
+
+/// OPERADORES MATEMATICOS o ARITMETICOS
