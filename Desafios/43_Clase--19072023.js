820df67b2cfce6ab2dd5792c13ff5bf489d14b68
Gaby <76602751+gagr-dev@users.noreply.github.com>
2023-07-19 19:57:16 -0300

    Desafios de la clase

---
+++ Desafios/43_Clase--19072023.js
@@ -0,0 +1,21 @@
+// 4. Sumatoria
+// Debés crear una función llamada `summation` que reciba un número como parámetro y que devuelva la sumatoria de todos sus números anteriores, incluso ese mismo.
+
+// Ejemplo:
+// summation(3) debe retornar 6 porque hace (1 +2 +3)
+// summation(8) debe retornar 36
+
+
+
+
+
+
+
+
+
+
+
+// 7. Carácter del medio
+// Debés crear una función llamada `middleCharacter` que reciba un `string` por parámetro y retorne sus caracteres del medio. Si la cantidad de caracteres en el string es impar, debería retornar su caracter medio. Si la cantidad de caracteres en el string es par, debería retornar los dos caracteres del medio.
+
+
