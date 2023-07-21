1a636672e150fc7f94ef95c62b98582b4739190b
Gaby <76602751+gagr-dev@users.noreply.github.com>
2023-07-21 19:05:07 -0300

    Desafios de la clase

---
+++ Desafios/44_Clase--21072023.js
@@ -0,0 +1,39 @@
+// 9. Manejar dos arreglos
+// Debés crear una función llamada `arrayHandler` que reciba dos arreglos de igual largo como parámetros y muestre en la consola “Soy {elemento de array 1} y yo soy {elemento de array 2}”.
+
+// Ejemplo:
+// arrayHandler([1,2,3,4], [“h”,”o”,”l”,”a”]) debe mostrar: 
+// Soy 1 y yo soy h
+// Soy 2 y yo soy o
+// Soy 3 y yo soy l
+// Soy 4 y yo soy a
+
+
+
+
+
+
+// 10. Mezclar arreglos
+// Debés crear una función llamada `mezclarArreglos` que reciba dos arreglos como parámetros y devuelva uno nuevo con los elementos de ambos, de 1 en 1.
+
+// Ejemplo: 
+// mezclarArreglos([1,2,3,4],[“h”,”o”,”l”,”a”]) debe retornar [1, “h”, 2,”o”, 3,”l”,4,”a”]
+// mezclarArreglos([1,2,3,4], [“h”,”p”]) debe retornar [1, “h”, 2, “p”, 3 , 4]
+// mezclarArreglos([“h”,”p”], [1,2,3,4]) debe retornar [“h”, 1, “p”, 2, 3 , 4]
+
+
+
+
+
+
+
+// 16. Guerra de palabras
+//Utils: 
+//var abc = //{a:1,b:2,c:3,d:4,e:5,f:6,g:7,h:8,i:9,j:10,k:11,l:12,m:13,n:14,o:15,p:16,q:17,r:18,s:19,t:20,u:21,v:22,w:23,x:24,y:25,z:26}
+
+
+// Debés crear una función llamada `warWords` que reciba dos palabras como parámetro y devuelva la ganadora según la suma del valor de sus letras (dado por su posición en el abecedario).
+
+// Ejemplo:
+// warWord(“hola”, “chau”) debe retornar  “hola” (36 > 33)
+// warWord(“love”, “friendship”) debe retornar “friendship”
